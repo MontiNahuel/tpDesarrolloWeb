e6142668b3eb23556da2e1ff301b0a5730b7e446
José <91619434+JoseGomez310@users.noreply.github.com>
2023-06-26 20:47:36 -0300

    Se creó el archivo jquery para las funciones

---
+++ jquery.js
@@ -0,0 +1,126 @@
+$(document).ready(function(){
+    //varios
+  $("#medialuna").hover(function(){
+    $("#pmedialuna").animate({
+        height: 'toggle'
+    })
+  })
+  $("#medialunajyq").hover(function(){
+    $("#pmedialunajyq").animate({
+        height: 'toggle'
+    })
+  })
+  $("#donas").hover(function(){
+    $("#pdonas").animate({
+        height: 'toggle'
+    })
+  })
+  $("#muffin").hover(function(){
+    $("#pmuffin").animate({
+        height: 'toggle'
+    })
+  })
+  $("#tostado").hover(function(){
+    $("#ptostado").animate({
+        height: 'toggle'
+    })
+  })
+  $("#chipa").hover(function(){
+    $("#pchipa").animate({
+        height: 'toggle'
+    })
+  })
+  $("#chocoBrownie").hover(function(){
+    $("#pchocoBrownie").animate({
+        height: 'toggle'
+    })
+  })
+  $("#cheesecake").hover(function(){
+    $("#pcheesecake").animate({
+        height: 'toggle'
+    })
+  })
+  $("#lemonpie").hover(function(){
+    $("#plemonpie").animate({
+        height: 'toggle'
+    })
+  })
+  $("#alfajor").hover(function(){
+    $("#palfajor").animate({
+        height: 'toggle'
+    })
+  })
+  $("#vasosPlastico").hover(function(){
+    $("#pvasosPlastico").animate({
+        height: 'toggle'
+    })
+  })
+  $("#vasosTermico").hover(function(){
+    $("#pvasosTermico").animate({
+        height: 'toggle'
+    })
+  })
+
+  //cafe
+  $("#cappuccino").hover(function(){
+    $("#pcappuccino").animate({
+        height: 'toggle'
+    })
+  })
+  $("#mocha").hover(function(){
+    $("#pmocha").animate({
+        height: 'toggle'
+    })
+  })
+  $("#lungo").hover(function(){
+    $("#plungo").animate({
+        height: 'toggle'
+    })
+  })
+  $("#cortado").hover(function(){
+    $("#pcortado").animate({
+        height: 'toggle'
+    })
+  })
+  $("#americano").hover(function(){
+    $("#pamericano").animate({
+        height: 'toggle'
+    })
+  })
+  $("#macchiato").hover(function(){
+    $("#pmacchiato").animate({
+        height: 'toggle'
+    })
+  })
+  $("#ristretto").hover(function(){
+    $("#pristretto").animate({
+        height: 'toggle'
+    })
+  })
+  $("#irish").hover(function(){
+    $("#pirish").animate({
+        height: 'toggle'
+    })
+  })
+  $("#cafeconleche").hover(function(){
+    $("#pcafeconleche").animate({
+        height: 'toggle'
+    })
+  })
+  $("#afogatto").hover(function(){
+    $("#pafogatto").animate({
+        height: 'toggle'
+    })
+  })
+  $("#flatwhite").hover(function(){
+    $("#pflatwhite").animate({
+        height: 'toggle'
+    })
+  })
+  $("#expresso").hover(function(){
+    $("#pexpresso").animate({
+        height: 'toggle'
+    })
+  })
+  
+})
